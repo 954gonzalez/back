594943c951d302e47957922f850456571d844e35
Jesus Antonio Gonzalez <114256805+954gonzalez@users.noreply.github.com>
2023-08-22 15:22:28 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 
 var app = express()
-.use(cors({credentials: true, origin: ''
+.use(cors({credentials: true, origin: '*'
 }))
 .use(bodyParser.json())
 .use(bodyParser.urlencoded({ extended: true })); 
